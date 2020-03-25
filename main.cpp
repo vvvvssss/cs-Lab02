18ddf96b1d79709370e3784eedc5d9660e29b467
Alice (ShepelinVA) <alice@example.com>
2020-03-25 13:51:36 +0300

    code: разность чисел

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "A + B = " << a + b << '\n';
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n';
     return 0;
 }
